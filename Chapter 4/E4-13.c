f366a64727d68be756cd1eae2d3529fc227e72a0
Prince Modi <princemody1122001@gmail.com>
2021-08-27 21:45:43 +0530

    Changed function name to revstr()

--- Chapter 4/E4-13.c
+++ Chapter 4/E4-13.c
@@ -9,23 +9,23 @@
 #include <stdio.h>
 #include <string.h>
 
-void rrev(char [], int, int);
+void revstr(char [], int, int);
 void mswap(char [], int, int);
 
 int main(){
     char str[]="It works?";
     int len=(int)strlen(str);
-    rrev(str,0,len-1
+    revstr(str,0,len-1
          );
     printf("%s\n",str);
     return 0;
 }
 
-void rrev(char str[], int i, int len){
+void revstr(char str[], int i, int len){
     if(i>=len)
         return;
     if(len>0){
-        rrev(str, i+1, len-1);
+        revstr(str, i+1, len-1);
     }
     mswap(str, len, i);
 }
