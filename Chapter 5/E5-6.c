65c2c3c777e2d55f6a614bf032234792b8d9189e
Prince Modi <princemody1122001@gmail.com>
2021-09-07 21:38:36 +0530

    Empty File

---
+++ Chapter 5/E5-6.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c project
+//
+//  Created by Prince Bhagirath Modi on 07/09/21.
+//  Rewrite appropriate programs from earliesr chapters and exercises with pointers instead of array indexing. Good possiblites
+//  include getline, atoi, itoa, and their variants, reverse, and strindex and getop.
+//
+//
+
+#include <stdio.h>
+/*
+ Will implement this functions for practice later.
+ */
+
